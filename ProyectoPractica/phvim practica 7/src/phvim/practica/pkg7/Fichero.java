8814ecc61f78fab6bf5ec258109d78cf5cbd005a
elMaromo <juampats12345@gmail.com>
2018-12-15 21:12:34 +0100

    primera grabacion y esqueleto del proyecto

---
+++ ProyectoPractica/phvim practica 7/src/phvim/practica/pkg7/Fichero.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package phvim.practica.pkg7;
+
+/**
+ *
+ * @author Juan
+ */
+public class Fichero
+{
+    
+}
---
+++ ProyectoPractica/phvim practica 7/src/phvim/practica/pkg7/Sonido.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package phvim.practica.pkg7;
+
+/**
+ *
+ * @author Juan
+ */
+public class Sonido
+{
+
+    String texto;
+    int[] sonidos;
+
+    Sonido(String textoAux)
+    {
+        sonidos = new int[textoAux.length() / 2];
+    }
+
+    //
+    void concatena()
+    {
+
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+
+}
